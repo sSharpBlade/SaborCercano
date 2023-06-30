f1f1b83a7bea1cbdfc4c77245c1c87d7c440dd1d
= <=>
2023-06-30 00:45:50 -0500

    modified:   carrito.php

--- carrito.php
+++ carrito.php
@@ -9,9 +9,9 @@ if (isset($_POST['id'])) {
     if ($token == $token_tmp) {
         if (isset($_SESSION['carrito']['productos'][$id])) {
             $_SESSION['carrito']['productos'][$id] += 1;
+        } else {
+            $_SESSION['carrito']['productos'][$id] = 1;
         }
-        $_SESSION['carrito']['productos'][$id] = 1;
-
         $datos['numero'] = count($_SESSION['carrito']['productos']);
         $datos['ok'] = true;
     } else {
