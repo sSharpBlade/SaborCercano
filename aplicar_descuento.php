de45e71c903bf862d438b5bf5b2634169a7573d5
= <=>
2023-07-05 07:28:43 -0500

    modified:   aplicar_descuento.php

--- aplicar_descuento.php
+++ aplicar_descuento.php
@@ -7,39 +7,40 @@ if (isset($_POST['codigo_descuento'])) {
         // Redirigir directamente a pago.php
         header('Location: pago.php');
         exit();
-    }
+    } else {
 
-    // Conectarse a la base de datos
-    include 'php/conexion.php';
-    require 'php/config.php';
-    $db = new DataBase();
-    $con = $db->conectar();
+        // Conectarse a la base de datos
+        include 'php/conexion.php';
+        require 'php/config.php';
+        $db = new DataBase();
+        $con = $db->conectar();
 
-    // Consultar la base de datos para validar el código de descuento
-    $sql = $con->prepare("SELECT * FROM descuentos WHERE codigo = ?");
-    $sql->execute([$codigo]);
-    $descuento = $sql->fetch(PDO::FETCH_ASSOC);
+        // Consultar la base de datos para validar el código de descuento
+        $sql = $con->prepare("SELECT * FROM descuentos WHERE codigo = ?");
+        $sql->execute([$codigo]);
+        $descuento = $sql->fetch(PDO::FETCH_ASSOC);
 
-    // Verificar si el código de descuento existe en la base de datos
-    if ($descuento) {
-        // Aplicar el descuento al total de la compra
-        $porcentaje_descuento = $descuento['porcentaje'];
-        $total = $_SESSION['total']; // Obtener el total de la compra de la sesión
-        $descuento_aplicado = ($porcentaje_descuento / 100) * $total;
-        $total_con_descuento = $total - $descuento_aplicado;
-        // Guardar el descuento aplicado en la sesión
-        $_SESSION['total'] = $total_con_descuento;
-        // Redirigir de vuelta a la página del carrito
-        echo '<script>
+        // Verificar si el código de descuento existe en la base de datos
+        if ($descuento) {
+            // Aplicar el descuento al total de la compra
+            $porcentaje_descuento = $descuento['porcentaje'];
+            $total = $_SESSION['total']; // Obtener el total de la compra de la sesión
+            $descuento_aplicado = ($porcentaje_descuento / 100) * $total;
+            $total_con_descuento = $total - $descuento_aplicado;
+            // Guardar el descuento aplicado en la sesión
+            $_SESSION['total'] = $total_con_descuento;
+            // Redirigir de vuelta a la página del carrito
+            echo '<script>
     alert("Código de descuento válido");
     window.location = "pago.php";
     </script>';
-        //header('Location: pago.php');
-        exit();
-    } else {
-        echo '<script>
+            //header('Location: pago.php');
+            exit();
+        } else {
+            echo '<script>
     alert("Código de descuento no válido");
     window.location = "checkout.php";
     </script>';
+        }
     }
 }
