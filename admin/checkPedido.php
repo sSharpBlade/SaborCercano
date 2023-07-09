ab4ad31d8e2125ec8590848a4c8261d77e50ddad
= <=>
2023-07-08 22:00:22 -0500

    modified:   admin/checkPedido.php

--- admin/checkPedido.php
+++ admin/checkPedido.php
@@ -5,7 +5,7 @@ $id = $_GET['Id'];
 if (isset($id)) {
     $db = new DataBase();
     $con = $db->conectar();
-    $stmt = $con->prepare("UPDATE `saborcercano`.`pedidos` SET `estado` = '1' WHERE (`id` = '$id');");
+    $stmt = $con->prepare("UPDATE `pedidos` SET `estado` = '1' WHERE (`id` = '$id');");
     $stmt->execute();
 }
 header("location:admin_pedidos.php");
